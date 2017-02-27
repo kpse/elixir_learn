3a765b0f2fdb012bd1726b9b2f9752f8e7b002ea
Suo Qin <kpsesuo@gmail.com>
2017-02-27 22:36:40 +1100

    add cli to run command

---
+++ cli.sh
@@ -0,0 +1,1 @@
+docker run -it -v $(pwd):/root -w /root --rm elixir iex 
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -v $(pwd):/root -w /root elixir elixir $1
+docker run -v $(pwd):/root -w /root --rm elixir elixir $1
